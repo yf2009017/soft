aaad2c0271cdb2372b9077d4b501d889a1f697a1
yf2009017 <yangfeiy_f@126.com>
2015-01-06 09:06:40 +0000

    configure file

---
+++ config.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+tar -jxvf mpich.tar.bz
+tar -zxvf wget.tar.gz
+MPIDIR=$HOME/soft/mpich
+WGET=$HOME/soft/wget
+echo PATH=$MPIDIR/bin:$WGET/bin:$PATH >>/etc/environment
+echo LD_LIBRARY_PATH=$MPIDIR/lib:$LD_LIBRARY_PATH >> /etc/environment
+
+source /etc/environment
+chmod +x $MPIDIR/bin/*
+chmod +x $WGET/bin/*
+## test wget
+mpicc main.c -o Test -I$MPIDIR/include -L$MPIDIR/lib -lmpi
+mpirun -np 4 ./Test
